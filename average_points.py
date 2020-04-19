f9f1ed9df39cfd28d07e7e0f8bc467e4ed217457
Andres Lujan <andreslujandev@gmail.com>
2020-04-19 16:42:08 -0500

    WIP

---
+++ average_points.py
@@ -0,0 +1,56 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return '{}, {}'.format(self.x, self.y)        
+
+
+class Rect():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.avg_point = None
+    
+    def avg(self):
+        avg_point_x = (self.a.x + self.b.x) / 2
+        avg_point_y = (self.a.y + self.b.y) / 2
+        
+        # Creates new avg point
+        self.avg_point = Point(avg_point_x, avg_point_y)
+
+        return self.avg_point
+
+    def plot(self):
+        x_values = [self.a.x, self.avg_point.x]
+        y_values = [self.a.y, self.avg_point.y]
+        plt.plot(x_values, y_values, marker='o', color='b')
+
+        x_values = [self.avg_point.x, self.b.x]
+        y_values = [self.avg_point.y, self.b.y]
+        plt.plot(x_values, y_values, marker='o', color='b')
+
+        plt.title('Rect')
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.show()
+    
+    def __repr__(self):
+        return '({})----({})'.format(self.a, self.b)
+
+
+if __name__ == '__main__':
+    a = Point(3, 4)
+    b = Point(5.5, 0)
+
+    r = Rect(a, b)
+
+    r_avg = r.avg()
+
+    print(r)
+    print(r.avg_point)
+    r.plot()
